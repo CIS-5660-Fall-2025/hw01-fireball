83a7155f94bff9d29658afe6b9cdce676d728088
Jillian Rayca <143532365+JillRay973@users.noreply.github.com>
2025-09-25 17:09:20 -0400

    Revert "HW1 - Daniel Gerhardt"

--- Source/Fireball/Fireball.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Fireball.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Fireball, "Fireball" );
